96ace1549b27cbe4d73cdb2d8adb5be8fb55d74e
tarvasa <60059249+tarvasa@users.noreply.github.com>
2020-11-24 11:22:32 +0200

    branch with files cleaned empty of comments and not-functioning code. Use this branch for presentation if no more solutions for code or unit tests is found

--- todo/src/app.js
+++ todo/src/app.js
@@ -1,4 +1,3 @@
-import {bindable, PLATFORM} from 'aurelia-framework';
 
 export class App {
   constructor() {
@@ -9,19 +8,8 @@ export class App {
       { title: 'Wash the dishes', done: true },
       { title: 'Take the garbage', done: false }
     ];
-    //this.todos = [];
   }
 
- /* configureRouter(config, router) {
-    config.title = 'Todos';
-    config.options.pushState = true;
-    config.options.root = '/';
-    config.map([
-      { route: '',  moduleId: PLATFORM.moduleName('./resources/elements/addtodo'), name:'addtodo' }
-    ]);
-
-    this.router = router;
-  }*/
   addTodo() {
     if(this.todoDescription) {
       this.todos.push({
