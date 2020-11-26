96529f150aaf6139e480f016113bb75d55e8db2b
tarvasa <60059249+tarvasa@users.noreply.github.com>
2020-11-26 17:36:29 +0200

    added add todo-buttons and remove buttons render tests

--- todo/test/unit/todo-list.spec.js
+++ todo/test/unit/todo-list.spec.js
@@ -8,12 +8,41 @@ describe('TodoList', () => {
     { title: 'Item 2', done: false },
     { title: 'Item 3', done: true }
   ];
+  //const functions = require('../../src/resources/elements/todo-list.js');
 
   beforeEach(() => {
     component = StageComponent
     .withResources('../../src/resources/elements/todo-list')
     .inView('<todo-list todos.bind="todos"></todo-list>')
     .boundTo({ todos })
+  })
+/*
+  addTodo() {
+    if(this.todoDescription) {
+      this.todos.push({
+        title: this.todoDescription,
+        done: false
+      });
+      this.todoDescription = '';
+    }
+  }
+
+  removeTodo(todo) {
+    let index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+*/
+  //testaa, että add-button renderöityy
+  it('should render Add Todo button/form', done => {
+    component.create(bootstrap).then(() => {
+      const formElement = document.querySelector('.addbutton');
+      expect(formElement.innerHTML).toBe('Add Todo');
+      done();
+    }).catch(e => {
+      console.log(e.toString());
+    });
   })
 
   it('should render the todos', done => {
@@ -45,6 +74,41 @@ describe('TodoList', () => {
       console.log(e.toString());
     });
   })
+
+  it('should render Remove Todo button', done => {
+    component.create(bootstrap).then(() => {
+      const listElements = document.querySelectorAll('.removebutton');
+      expect(listElements[2].innerHTML).toBe('Remove');
+      done();
+    }).catch(e => {
+      console.log(e.toString());
+    });
+  })
+
+ /* it('should add a todo', done => {
+    component.create(bootstrap).then(() => {
+      let todoDescription = 'Item 4';
+      //todos.addTodo();
+      expect(functions.addTodo).toBeDefined();
+      todos.addTodo();
+      expect(todos.length).toBe(3);
+      //expect(todos.length).toBe(3);
+      done();
+    }).catch(e => {
+      console.log(e.toString());
+    });
+  })*/
+
+ /* it('should delettodo', done => {
+    component.create(bootstrap).then(() => {
+      
+      todos.removeTodo();
+      expect(todos.length).toBe();
+      done();
+    }).catch(e => {
+      console.log(e.toString());
+    });
+  })*/
 
   afterEach(() => {
     component.dispose();
